0c6a133d9a11eaa7b597009beab08306d08068d2
JMSB-LEARN <jmarquess04@iesalbarregas.es>
2025-10-16 18:11:38 +0200

    Update Base 1

---
+++ app/src/main/java/com/example/domoticon/ThreeBulbsActivity.java
@@ -0,0 +1,21 @@
+package com.example.domoticon;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.domoticon.databinding.ActivityMainBinding;
+
+public class ThreeBulbsActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+    }
+}
